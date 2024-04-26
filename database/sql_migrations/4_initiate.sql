797dab409d3da2d0f57286a0a21b9a28235e1878
abdulatif-fs <fasijardiq@gmail.com>
2024-04-26 21:25:16 +0700

    update sql

--- database/sql_migrations/4_initiate.sql
+++ database/sql_migrations/4_initiate.sql
@@ -4,7 +4,7 @@
 CREATE TABLE detail_transaksi(
     transaksi_id BIGINT REFERENCES transaksi (id) ,
     menu_id BIGINT REFERENCES menu (id),
-    quantity int,
+    quantity int
 )
 
 -- +migrate StatementEnd
